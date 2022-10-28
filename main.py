fa5effa8ffd626ee27f97a39aabd889d76ad6bdd
lucasbragaDH <52525909+lucasbragaDH@users.noreply.github.com>
2022-10-29 01:36:45 +0200

    changes in main

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
 import streamlit as st
 import pandas as pd
-from gsheetsdb import connect
-# import pygsheets
+from google.oauth2 import service_account
+import pygsheets
 
-st.title("My First Streamlit Web App")
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+client = pygsheets.authorize(custom_credentials=credentials)
+sheet = client.open_by_key('1hU-HwHPI61lHjAtECxsWJUjWQ1e_sSOeION72757pKs')
+wk = sheet.worksheet_by_title('Sheet1')
 
-df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
-st.write(df)
+st.title("Give your score prediction for Serbia vs. Brazil")
+user_input_name = st.text_area("What's your name") #, "Jorge Almeida")
+user_input1 = st.text_input("Serbia") #,0)
+user_input2 = st.text_input("Brazil") #,0)
+def input_from_user(wk, user_input_name, user_input1, user_input2):
+    dat = pd.DataFrame(sheet.worksheet_by_title('Sheet1').range('A2:C1000', returnas='matrix'), 
+        columns=['Name of the person', 'Team 1 score','Team 2 score'])
+    lastrow = int(dat[(dat['Name of the person']!='') &\
+                 (dat['Team 1 score']!='') & (dat['Team 2 score']!='')].shape[0]+1)
+    newrow = lastrow+1
+    return newrow
 
-st.title("Connect to Google Sheets")
-gsheet_url = st.secrets["public_gsheets_url"]
-conn = connect()
-rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-df_gsheet = pd.DataFrame(rows)
-st.write(df_gsheet)
+st.title("Predictions so far")
+
+def main(wk, user_input_name, user_input1, user_input2):
+    newrow = input_from_user(wk, user_input_name, user_input1, user_input2)
+    wk.update_values(f'A{newrow}:C{newrow}', 
+        values=[[user_input_name, user_input1, user_input2]],majordim='ROWS')
+    dat = pd.DataFrame(sheet.worksheet_by_title('Sheet1').range(f'A2:C{newrow}', returnas='matrix'), 
+        columns=['Name of the person', 'Team 1 score','Team 2 score'])
+    st.write(dat)
+
+if __name__ == "__main__":
+    main(wk, user_input_name, user_input1, user_input2)
