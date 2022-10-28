7f8f24fb08bd25a8f5a973e5e2db3655de247e50
lucasbragaDH <52525909+lucasbragaDH@users.noreply.github.com>
2022-10-28 21:09:48 +0200

    requirements added

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ st.title("My First Streamlit Web App")
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 
-# st.title("Connect to Google Sheets")
-# gsheet_url = st.secrets["public_gsheets_url"]
-# conn = connect()
-# rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-# df_gsheet = pd.DataFrame(rows)
-# st.write(df_gsheet)
+st.title("Connect to Google Sheets")
+gsheet_url = st.secrets["public_gsheets_url"]
+conn = connect()
+rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
+df_gsheet = pd.DataFrame(rows)
+st.write(df_gsheet)
